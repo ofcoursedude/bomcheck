c879c3235aae43f398209d9a19c3f1b1584292f8
Pavel Simsa <pavel@simsa.cz>
2022-02-19 20:36:54 +0100

    ...

--- src/bomcheck/Program.cs
+++ src/bomcheck/Program.cs
@@ -1,11 +1,12 @@
 using System.Buffers;
 using System.Text;
 
-if (args.Length != 2)
+if (args.Length == 0)
 {
-    Console.WriteLine("Usage: bomcheck[.exe] <root_folder> <autofix>");
-    Console.WriteLine("Examples: bomcheck.exe .\\repo false");
-    Console.WriteLine("          ./bomcheck ./repo false");
+    Console.WriteLine("Usage: bomcheck[.exe] <root_folder> [--autofix|-af]");
+    Console.WriteLine("Examples: bomcheck.exe .\\repo -autofix");
+    Console.WriteLine("          ./bomcheck ./repo");
+    Console.WriteLine("Note: Only files with size under approx. 2GB (int32) are supported.");
     return;
 }
 
@@ -18,7 +19,9 @@ if (!Directory.Exists(path))
 
 Console.WriteLine($"Will check {path}...");
 
-var autofix = args[1] == "true";
+var autofix = args.Any(x =>
+    x.Equals("--autofix", StringComparison.InvariantCultureIgnoreCase) ||
+    x.Equals("-af", StringComparison.InvariantCultureIgnoreCase));
 
 var enc = new UTF8Encoding(true);
 var preamble = enc.GetPreamble();
@@ -36,52 +39,53 @@ var files = Directory.GetFiles(path, "*.*", new EnumerationOptions()
     ReturnSpecialDirectories = false
 });
 
-await Parallel.ForEachAsync(files, async (file, ct) =>
-{
-    await using var fs = File.OpenRead(file);
-    if (fs.Length < preambleLength)
+await Parallel.ForEachAsync(files, new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount * 2 },
+    async (file, ct) =>
     {
-        Console.WriteLine($"File {file} is too small to contain a BOM, skipping...");
-        skippedFiles++;
-        return;
-    }
+        await using var fs = File.OpenRead(file);
+        if (fs.Length < preambleLength)
+        {
+            Console.WriteLine($"File {file} is too small to contain a BOM, skipping...");
+            skippedFiles++;
+            return;
+        }
 
-    var buf = ArrayPool<byte>.Shared.Rent(preambleLength);
-    await fs.ReadAsync(buf.AsMemory(0, preambleLength), ct);
-    var hasBom = buf[..preambleLength].SequenceEqual(preamble);
-    ArrayPool<byte>.Shared.Return(buf);
-    if (!hasBom) return;
+        var buf = ArrayPool<byte>.Shared.Rent(preambleLength);
+        await fs.ReadAsync(buf.AsMemory(0, preambleLength), ct);
+        var hasBom = buf[..preambleLength].SequenceEqual(preamble);
+        ArrayPool<byte>.Shared.Return(buf);
+        if (!hasBom) return;
 
-    Console.WriteLine($"File {file} has a BOM");
-    Interlocked.Increment(ref bomFiles);
-    if (!autofix) return;
+        Console.WriteLine($"File {file} has a BOM");
+        Interlocked.Increment(ref bomFiles);
+        if (!autofix) return;
 
-    if (fs.Length > int.MaxValue)
-    {
-        Console.WriteLine($"File {file} is too big to fix, skipping...");
-        Interlocked.Increment(ref skippedFiles);
-        return;
-    }
+        if (fs.Length > int.MaxValue)
+        {
+            Console.WriteLine($"File {file} is too big to fix, skipping...");
+            Interlocked.Increment(ref skippedFiles);
+            return;
+        }
 
-    if (fs.Length == preambleLength)
-    {
-        Console.WriteLine($"File {file} has a BOM but is empty, skipping...");
-        Interlocked.Increment(ref skippedFiles);
-        return;
-    }
+        if (fs.Length == preambleLength)
+        {
+            Console.WriteLine($"File {file} has a BOM but is empty, skipping...");
+            Interlocked.Increment(ref skippedFiles);
+            return;
+        }
 
-    var contentLength = (int)fs.Length;
-    var realLength = contentLength - preambleLength;
-    var bytes = ArrayPool<byte>.Shared.Rent(realLength);
-    fs.Seek(preambleLength, SeekOrigin.Begin);
-    await fs.ReadAsync(bytes.AsMemory(0, realLength), ct);
-    fs.Close();
+        var contentLength = (int)fs.Length;
+        var realLength = contentLength - preambleLength;
+        var bytes = ArrayPool<byte>.Shared.Rent(realLength);
+        fs.Seek(preambleLength, SeekOrigin.Begin);
+        await fs.ReadAsync(bytes.AsMemory(0, realLength), ct);
+        fs.Close();
 
-    await File.WriteAllBytesAsync(file, bytes[..realLength], ct);
-    ArrayPool<byte>.Shared.Return(bytes);
-    Console.WriteLine($"File {file} fixed.");
-    Interlocked.Increment(ref fixedFiles);
-});
+        await File.WriteAllBytesAsync(file, bytes[..realLength], ct);
+        ArrayPool<byte>.Shared.Return(bytes);
+        Console.WriteLine($"File {file} fixed.");
+        Interlocked.Increment(ref fixedFiles);
+    });
 
 Console.WriteLine($"Files with BOM: {bomFiles}");
 Console.WriteLine($"Fixed files:    {fixedFiles}");
